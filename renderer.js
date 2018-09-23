f0b77723fb7dd360c5a0bdff2e04f9336d43b07b
Kevinsal03 <kevinsal03@users.noreply.github.com>
2018-09-23 16:29:13 -0400

    some new deps

--- renderer.js
+++ renderer.js
@@ -1,3 +1,13 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+'use strict'
+
+window.$ = window.jQuery = require('jquery')
+window.Tether = require('tether')
+window.Bootstrap = require('bootstrap')
+
+//require bootstrap css
+$('head').append('<link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css">');
+//require font-awesome css
+$('head').append('<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">')
